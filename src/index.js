ae3e1ddca373311c45351059f978f4706febab9c
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-21 14:48:49 +0530

    gh-page version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="nasa-rocket-launch">
     <App />
   </BrowserRouter>
   , document.getElementById('root'));
