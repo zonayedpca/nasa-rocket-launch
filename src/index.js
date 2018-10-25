15066b25be25721c3a3c2863eba2a2aaf9223584
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-25 17:26:22 +0530

    default project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename="nasa-rocket-launch">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
   , document.getElementById('root'));
